715f9588fd6d226ad82a120064f4a6a727cf93e7
sabishi <jobericastro@gmail.com>
2024-08-01 02:29:10 -0500

    Avance del 2 al 4

--- tallerJava/src/taller2.java
+++ tallerJava/src/taller2.java
@@ -1,8 +1,24 @@
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class taller2 {
 
+
     public static void main(String[] args) {
+
+        // System.out.println(ejercicio2());
+        ejercicio4();
+    }
+
+    /*
+     * Realiza un algoritmo el cual me permita realizar el cálculo de una
+     * calculadora (suma, resta, multiplicación, división, residuo, potencia)
+     * y en el cual pueda elegir por el
+     * usuario la opción deseada
+     */
+
+    static void ejercicio1() {
+
         Scanner dei = new Scanner(System.in);
         int c = 0;
         int opciones = 0;
@@ -103,7 +119,105 @@ public class taller2 {
         } while (opciones == 7);
     }
 
-    public static void ejercicio2() {
+    /*
+     * Realiza un algoritmo el cual me muestre solo los números pares digitados
+     * por el usuario, use ciclo for
+     */
+    static String ejercicio2() {
+
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite n veces para pedir numero");
+        int n = input.nextInt();
+        String text = "Sus numeros pares son: \n";
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print(MessageFormat.format("Escriba el numero {0} deseado: ", i));
+            int number = input.nextInt();
+
+            text = (number % 2 == 0) ? text.concat(MessageFormat.format("{0} ", number)) : null;
+
+        }
+        return text;
+    }
+
+    /*
+     * Realiza un algoritmo el cual me permita realizar el cálculo de las tablas
+     * de multiplicar de un número, use ciclo while?
+     * Ejemplo: 1X1, 1X2, 1X3 ……. 1X10.
+     */
+
+    static String ejercicio3() {
+
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+        int i = 1;
+        String text = "Tabla Multiplicar\n";
+
+        System.out.print("Escriba el numero de la tabla: ");
+        int n = input.nextInt();
+
+        while (i <= 10) {
+
+            text = text.concat(MessageFormat.format("{0}x{1} = {2}\n", n, i, n * i));
+            i++;
+
+        }
+
+        return text;
 
     }
+
+    /*
+     * Realiza un algoritmo que me calcule el sueldo promedio de N empleados de
+     * una empresa, cuantos ganan más de un sueldo mínimo y cuantos ganan menos de
+     * ese sueldo, use ciclo for
+     */
+
+    static String ejercicio4() {
+
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite n empleados: ");
+        int n = input.nextInt();
+        int sueldoMinimo = 0;
+        int sueldoMaximo = 0;
+        Double totalDouble = 0d;
+        String text = "";
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print(MessageFormat.format("Digite sueldo del empleado #{0}: ", i));
+            int sueldo = input.nextInt();
+
+            totalDouble += sueldo;
+
+            sueldoMaximo = Math.max(sueldoMaximo, sueldo);
+            sueldoMinimo = Math.min(sueldoMaximo, sueldo);
+
+        }
+
+        text = text.concat(
+                String.format("%1$s %4$,.0f %2$s %5$,d %3$s %6$,d",
+                        "El sueldo promedio es ", "\nEl sueldo maximo es ",
+                        "\nEl sueldo minimo es ", totalDouble / n, sueldoMaximo, sueldoMinimo));
+
+        return text;
+
+    }
+
+    /*
+     * 5. ¿Realice un algoritmo que me calcule la nota promedio de varios
+     * estudiantes con N
+     * materias, use ciclo while?
+     */
+
+    /*
+     * 6. Realice un algoritmo que me muestre el cálculo, de las bonificaciones y
+     * sueldo total de N
+     * trabajadores, la fórmula del sueldo total seria la siguiente, use ciclos
+     */
 }
