4895a053485f6d023bd5a68449a49e1df9c387f4
sabishi <jobericastro@gmail.com>
2024-08-02 01:51:30 -0500

    Taller terminado Falta revision

--- tallerJava/src/taller2.java
+++ tallerJava/src/taller2.java
@@ -1,13 +1,66 @@
 import java.text.MessageFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class taller2 {
 
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        // System.out.println(ejercicio2());
-        ejercicio4();
+        Integer opcion = 0;
+
+        try {
+
+            do {
+
+                System.out.println("Numero del ejercicio     o 0 para finalizar programa");
+
+                if (input.hasNextInt()) {
+                    opcion = input.nextInt();
+                } else {
+                    System.out.println("Error en el escaner");
+                }
+                switch (opcion) {
+                    case 1:
+                        System.out.println(ejercicio1());
+                        Thread.sleep(3000);
+                        break;
+                    case 2:
+                        System.out.println(ejercicio2());
+                        Thread.sleep(3000);
+                        break;
+                    case 3:
+                        System.out.println(ejercicio3());
+                        Thread.sleep(3000);
+                        break;
+                    case 4:
+                        System.out.println(ejercicio4());
+                        Thread.sleep(3000);
+                        break;
+                    case 5:
+                        System.out.println(ejercicio5());
+                        Thread.sleep(3000);
+                        break;
+                    case 6:
+                        System.out.println(ejercicio6());
+                        Thread.sleep(3000);
+                        break;
+                    case 0:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Por favor, elija un número entre 1 y 6 o 0 para salir.");
+                        break;
+                }
+            } while (opcion != 0);
+
+            input.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 
     /*
@@ -17,106 +70,65 @@ public class taller2 {
      * usuario la opción deseada
      */
 
-    static void ejercicio1() {
+    static String ejercicio1() {
 
-        Scanner dei = new Scanner(System.in);
-        int c = 0;
-        int opciones = 0;
-        int resultado = 0;
+        int opcion = 0;
+        String text = "";
 
-        do {
+        try {
+            System.out.println("1.sumar\n2.restar\n3.Multiplicar\n4.Dividir\n5.Potencia\n6.Residuo");
+            System.out.print("Opcion: ");
+            opcion = input.nextInt();
 
-            System.out.println("1.sumar");
-            System.out.println("2.restar");
-            System.out.println("3.multiplicar");
-            System.out.println("4.dividir");
-            System.out.println("5.potenciar");
-            System.out.println("6.residuo");
-            System.out.println("7.salir");
+            System.out.print("Digite n1: ");
+            Integer n1 = input.nextInt();
+            System.out.print("Digite n2: ");
+            Integer n2 = input.nextInt();
 
-            opciones = dei.nextInt();
-
-            switch (opciones) {
+            switch (opcion) {
                 case 1:
-                    System.out.println("suma");
-                    System.out.println("-------------------");
-                    System.out.println("digite numero #1");
-                    int n1 = dei.nextInt();
 
-                    System.out.println("digite numero #1");
-                    int n111 = dei.nextInt();
-
-                    System.out.println("la suma es : " + resultado);
-
+                    text = text.concat(String.format("%1$,d + %2$,d = %3$,d", n1, n2, n1 + n2));
                     break;
-
                 case 2:
-                    System.out.println("resta");
-                    System.out.println("-------------------");
-                    System.out.println("digite numero #1");
-                    int n2 = dei.nextInt();
-                    System.out.println("digite numero #2 ");
-                    int n3 = dei.nextInt();
 
-                    System.out.println("la resta es : " + resultado);
+                    text = text.concat(String.format("%1$,d - %2$,d = %3$,d", n1, n2, n1 - n2));
                     break;
                 case 3:
-                    System.out.println(" operacion multiplicacion");
 
-                    System.out.println("-------------------");
-
-                    System.out.println("digite numero #1");
-
-                    int n5 = dei.nextInt();
-                    System.out.println("digite numero #2 ");
-                    int n4 = dei.nextInt();
-
-                    System.out.println("la multiplicacion es : " + resultado);
+                    text = text.concat(String.format("%1$,d * %2$,d = %3$,d", n1, n2, n1 * n2));
                     break;
                 case 4:
-                    System.out.println("divicion");
-                    System.out.println("-------------------");
-                    System.out.println("digite numero #1");
-                    int n6 = dei.nextInt();
-                    System.out.println("digite numero #2 ");
-                    int n7 = dei.nextInt();
-                    if (n7 == 0) {
-                        System.out.println("error no se puede dividir por 0");
-                    } else {
-                        System.out.println("la divicion es  : " + resultado);
-                    }
 
+                    if (n1 == 0 || n2 == 0) {
+                        text = "Datos Invalidos!";
+                        break;
+                    }
+                    text = text.concat(String.format("%1$,d / %2$,d = %3$,d", n1, n2, n1 / n2));
                     break;
                 case 5:
-                    System.out.println("operacion potencia ");
-                    System.out.println("-------------------");
-                    System.out.println("digite numero a potenciar ");
-                    double n8 = dei.nextInt();
 
-                    System.out.println("digite numero de la elevacion ");
-                    double n9 = dei.nextInt();
-
-                    System.out.println("la potencia es  : " + resultado);
+                    text = text.concat(String.format("%1$,d ^ %2$,d = %3$,d", n1, n2, n1 ^ n2));
                     break;
                 case 6:
-                    System.out.println("operacion residuo");
-                    System.out.println("-------------------");
-                    System.out.println("digite numero #1");
-                    int n10 = dei.nextInt();
-                    System.out.println("digite numero #2 ");
-                    int n11 = dei.nextInt();
 
-                    System.out.println("el residuo es  : " + resultado);
+                    text = text.concat(String.format("%1$,d % %2$,d = %3$,d", n1, n2, n1 % n2));
                     break;
                 case 7:
-                    c = 100;
-                    break;
-                default:
-                    System.out.println("la hemos liado digite una opcion ");
+
+                    text = "......Saliendo del programa......";
+                    Thread.sleep(2000);
                     break;
 
+                default:
+                    System.out.println("Opcion Invalida!");
+                    break;
             }
-        } while (opciones == 7);
+
+        } catch (Exception e) {
+            text = String.format("Excepcion: %1$s", e);
+        }
+        return text;
     }
 
     /*
@@ -125,10 +137,7 @@ public class taller2 {
      */
     static String ejercicio2() {
 
-        @SuppressWarnings("resource")
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Digite n veces para pedir numero");
+        System.out.print("Digite n veces para pedir numero: ");
         int n = input.nextInt();
         String text = "Sus numeros pares son: \n";
 
@@ -137,7 +146,7 @@ public class taller2 {
             System.out.print(MessageFormat.format("Escriba el numero {0} deseado: ", i));
             int number = input.nextInt();
 
-            text = (number % 2 == 0) ? text.concat(MessageFormat.format("{0} ", number)) : null;
+            text = (number % 2 == 0) ? text.concat(MessageFormat.format("{0}, ", number)) : null;
 
         }
         return text;
@@ -151,8 +160,6 @@ public class taller2 {
 
     static String ejercicio3() {
 
-        @SuppressWarnings("resource")
-        Scanner input = new Scanner(System.in);
         int i = 1;
         String text = "Tabla Multiplicar\n";
 
@@ -178,14 +185,15 @@ public class taller2 {
 
     static String ejercicio4() {
 
-        @SuppressWarnings("resource")
-        Scanner input = new Scanner(System.in);
-
         System.out.print("Digite n empleados: ");
         int n = input.nextInt();
+
         int sueldoMinimo = 0;
         int sueldoMaximo = 0;
+
         Double totalDouble = 0d;
+
+        String format = "El sueldo promedio es %1$,.0f\nEl sueldo maximo es %2$,d\nEl sueldo minimo es %3$,d";
         String text = "";
 
         for (int i = 1; i <= n; i++) {
@@ -201,9 +209,7 @@ public class taller2 {
         }
 
         text = text.concat(
-                String.format("%1$s %4$,.0f %2$s %5$,d %3$s %6$,d",
-                        "El sueldo promedio es ", "\nEl sueldo maximo es ",
-                        "\nEl sueldo minimo es ", totalDouble / n, sueldoMaximo, sueldoMinimo));
+                String.format(format, totalDouble / n, sueldoMaximo, sueldoMinimo));
 
         return text;
 
@@ -211,13 +217,100 @@ public class taller2 {
 
     /*
      * 5. ¿Realice un algoritmo que me calcule la nota promedio de varios
-     * estudiantes con N
-     * materias, use ciclo while?
+     * estudiantes con N materias, use ciclo while?
      */
+    static String ejercicio5() {
+
+        input.useLocale(Locale.US);
+
+        System.out.print("Digite n estudiantes: ");
+        int nEstudiantes = input.nextInt();
+
+        Double totalNota = 0d;
+        String text = "";
+
+        int i = 1;
+        while (i <= nEstudiantes) {
+
+            System.out.print(MessageFormat.format("Escriba n materia para el estudiante {0}: ", i));
+            int nMaterias = input.nextInt();
+
+            for (int x = 1; x <= nMaterias; x++) {
+
+                System.out.print(MessageFormat.format("Digite la nota del estudiante {01} materia {1}: ", i, x));
+                double nota = input.nextDouble();
+                totalNota = Double.sum(totalNota, nota);
+
+            }
+
+            text = text
+                    .concat(String.format("El promedio del estudiante %1$d es %2$.1f\n", i,
+                            totalNota / nMaterias));
+            totalNota = 0d;
+            i++;
+
+        }
+
+        return text;
+
+    }
 
     /*
      * 6. Realice un algoritmo que me muestre el cálculo, de las bonificaciones y
-     * sueldo total de N
-     * trabajadores, la fórmula del sueldo total seria la siguiente, use ciclos
+     * sueldo total de N trabajadores, la fórmula del sueldo total seria la
+     * siguiente, use ciclos
+     * 
+     * Tipo empleado Bonificación Formula General
+     * A 20% Sueldo=Sueldo + (Sueldo*20%)
+     * B 30% Sueldo=Sueldo + (Sueldo*30%)
+     * C 40% Sueldo=Sueldo + (Sueldo*40%)
      */
+    static String ejercicio6() {
+
+        System.out.print("Digite n trabajadores: ");
+        int nTrabajadores = input.nextInt();
+        input.nextLine();
+
+        String format = "Trabajador %1$d es tipo %2$c con sueldo %3$,.1f aplicando bonificacion %4$,.1f\n";
+        String text = "";
+
+        for (int i = 1; i <= nTrabajadores; i++) {
+
+            System.out.print(MessageFormat.format("Digite el tipo del trabjador #{0}: ", i));
+            Character tipoEmpleado = input.nextLine().charAt(0);
+
+            System.out.print(MessageFormat.format("Digite el sueldo del trabjador #{0}: ", i));
+            Double sueldo = input.nextDouble();
+            input.nextLine();
+
+            switch (Character.toLowerCase(tipoEmpleado)) {
+                case 'a':
+
+                    text = text.concat(String.format(format, i, Character.toUpperCase(tipoEmpleado), sueldo,
+                            sueldo + (sueldo * 0.2)));
+                    break;
+
+                case 'b':
+
+                    text = text.concat(String.format(format, i, Character.toUpperCase(tipoEmpleado), sueldo,
+                            sueldo + (sueldo * 0.3)));
+                    break;
+
+                case 'c':
+
+                    text = text.concat(String.format(format, i, Character.toUpperCase(tipoEmpleado), sueldo,
+                            sueldo + (sueldo * 0.4)));
+                    break;
+
+                default:
+
+                    System.out.println("Datos Invalidos");
+                    break;
+            }
+
+        }
+        return text;
+
+    }
+
 }
